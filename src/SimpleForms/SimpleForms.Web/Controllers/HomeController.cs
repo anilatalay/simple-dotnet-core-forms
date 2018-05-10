605eeb395252ca4adac4aca0ec9d60144463995f
Anıl Atalay <anilatalay@hotmail.com.tr>
2018-05-10 21:53:39 +0300

    Added simple mvc

---
+++ src/SimpleForms/SimpleForms.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleForms.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
